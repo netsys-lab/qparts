6e8d7001ab3b852ee558c21671bf12226310075a
Marten Gartner <martenwallewein@web.de>
2024-10-24 14:28:50 +0200

    Add fileserver example for testing the reverse site

--- doc/TODOS.md
+++ doc/TODOS.md
@@ -1,14 +1,14 @@
 # Main TODOS
 
+- Proper Logging
 - Proper error handling
 - Proper number of QUICDataplaneStreams
 - Do proper logging
 - Hide quic go not a udp conn message
 - Return proper numbers of Read/Write from API
-- Communicate paths from conn to QUICDataplaneStream for reverse scheduling
+- [x] Communicate paths from conn to QUICDataplaneStream for reverse scheduling
 - Implement Deadline functionalities
 - Stream Types
-- Reliability / Retransfers
 - Priorities
 - ReadLarge/WriteLarge Methods
 - Crypto
--- examples/fileserver/main.go
+++ examples/fileserver/main.go
@@ -44,6 +44,7 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
+
 		fmt.Println("Received ", n, " bytes for size")
 		size = binary.BigEndian.Uint64(sizeData)
 		buf := make([]byte, size)
@@ -53,8 +54,42 @@ func main() {
 			log.Fatal(err)
 		}
 
-		fmt.Println("Received ", n, " bytes via stream")
+		file := string(buf)
+
+		fileInfo, err := os.Stat(file)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fileSize := fileInfo.Size()
+		// Read file into bytes
+		buf = make([]byte, fileSize)
+		filePtr, err := os.Open(file)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		_, err = io.ReadFull(filePtr, buf)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fileSizeData := make([]byte, 8)
+		binary.BigEndian.PutUint64(fileSizeData, uint64(fileSize))
+
+		n, err = stream.Write(fileSizeData)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		n, err = stream.Write(buf)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("-------------------------------")
 		fmt.Printf("Hash %x\n", sha256.Sum256(buf))
+		fmt.Println("Sent ", n, " bytes via stream")
+		time.Sleep(1 * time.Second)
 
 		// }
 
@@ -76,12 +111,7 @@ func main() {
 
 		fmt.Println("Opened stream")
 
-		fileInfo, err := os.Stat(file)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		size := fileInfo.Size()
+		size := len(file)
 		sizeData := make([]byte, 8)
 		binary.BigEndian.PutUint64(sizeData, uint64(size))
 
@@ -92,25 +122,31 @@ func main() {
 		fmt.Println("Sent ", n, " bytes for size")
 
 		// Read file into bytes
-		buf := make([]byte, size)
-		filePtr, err := os.Open(file)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		_, err = io.ReadFull(filePtr, buf)
-		if err != nil {
-			log.Fatal(err)
-		}
-
+		buf := []byte(file)
 		n, err = stream.Write(buf)
 		if err != nil {
 			log.Fatal(err)
 		}
 
+		var fileSize uint64
+		fileSizeData := make([]byte, 8)
+		n, err = stream.Read(fileSizeData)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println("Received ", n, " bytes for size")
+		fileSize = binary.BigEndian.Uint64(fileSizeData)
+		buf = make([]byte, fileSize)
+
+		n, err = io.ReadFull(stream, buf)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println("-------------------------------")
+		fmt.Println("Received ", n, " bytes via stream")
 		fmt.Printf("Hash %x\n", sha256.Sum256(buf))
-		fmt.Println("Sent ", n, " bytes via stream")
-		time.Sleep(1 * time.Second)
 
 	}
 

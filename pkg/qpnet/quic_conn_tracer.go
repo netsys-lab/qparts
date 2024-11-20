c3c68438ea649fc97fff246bf2e8387d541f3e29
Marten Gartner <martenwallewein@web.de>
2024-11-20 09:54:35 +0100

    Add logging for quic conn tracer

--- pkg/qpnet/quic_conn_tracer.go
+++ pkg/qpnet/quic_conn_tracer.go
@@ -3,6 +3,7 @@ package qpnet
 import (
 	"context"
 
+	"github.com/netsys-lab/qparts/pkg/qplogging"
 	"github.com/netsys-lab/qparts/pkg/qpscion"
 	"github.com/quic-go/quic-go"
 	"github.com/quic-go/quic-go/logging"
@@ -31,12 +32,29 @@ func (qt *QPartsQuicTracer) NewTracerHandler() func(context context.Context, per
 		qt.ConnectionID = connectionID
 
 		qt.Tracer = &logging.ConnectionTracer{}
-		qt.Tracer.AcknowledgedPacket = func(encLevel logging.EncryptionLevel, packetNumber logging.PacketNumber) {
-			// qplogging.Log.Debug("Acked Packet")
+		qt.Tracer.UpdatedCongestionState = func(state logging.CongestionState) {
+			if state == logging.CongestionStateRecovery {
+				qplogging.Log.Info("Received recovery state on connection ", connectionID)
+				if qt.Path != nil {
+					qplogging.Log.Info("Path: ", qt.Path)
+				}
+			}
 		}
 		qt.Tracer.LostPacket = func(encLevel logging.EncryptionLevel, packetNumber logging.PacketNumber, reason logging.PacketLossReason) {
-			// qplogging.Log.Debug("Lost Packet")
-			// qplogging.Log.Debug(reason)
+			qplogging.Log.Info("Lost Packet with reason ", reason, " on conn ", connectionID)
+			if qt.Path != nil {
+				qplogging.Log.Info("Path: ", qt.Path)
+			}
+		}
+		qt.Tracer.UpdatedMetrics = func(rttStats *logging.RTTStats, cwnd, bytesInFlight logging.ByteCount, packetsInFlight int) {
+			qplogging.Log.Info("Updated Metrics: ")
+			qplogging.Log.Info("RTT Stats: ", rttStats)
+			qplogging.Log.Info("CWND: ", cwnd)
+			qplogging.Log.Info("Bytes in Flight: ", bytesInFlight)
+			qplogging.Log.Info("Packets in Flight: ", packetsInFlight)
+			if qt.Path != nil {
+				qplogging.Log.Info("Path: ", qt.Path)
+			}
 		}
 		return qt.Tracer
 	}
